360ef1a2d7aa921c93964dfa81f228dd7a1fad1b
Sindre Wetjen <sindre.w@gmail.com>
2012-04-02 18:41:49 +0200

    tex filer

---
+++ tex/re1.py
@@ -0,0 +1,7 @@
+# Hent den koden vi trenger fra
+# python sin egen kodebase.
+import re
+
+# Et funksjonskall for  
+# sok etter det vi vil ha i en tekst.
+re.search(det_vi_vil_ha, tekst)
---
+++ tex/re2.py
@@ -0,0 +1,7 @@
+# Hent den koden vi trenger fra
+# python sin egen kodebase.
+import re
+
+# Let etter uttrykket i en tekst.
+re.search(uttrykk, tekst)
+
