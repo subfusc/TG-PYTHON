8359f194788229a77dc886f6b712801b347df318
Sindre Wetjen <sindre.w@gmail.com>
2013-03-23 13:30:47 +0100

    Preparing for a new year of tg with new course codes

--- PY1000/hello_world.py
+++
@@ -1,5 +0,0 @@
-# Det finnes mange versjoner av Python.
-# I dette kurset bruker jeg Python 2.7
-# Alle programmeringsguider starter med "Hello World!", og dette er intet unntak
-
-print 'hello World!'
--- PY1000/loops.py
+++
@@ -1,9 +0,0 @@
-# coding=utf8
-# Det finnes mange versjoner av Python.
-# I dette kurset bruker jeg Python 2.7
-
-kursliste = ['PY1000', 'PY2000', 'REG1000']
-
-for kurs in kursliste:
-    print kurs
-
